4edb66ddc3efbbbca6d1f2528d43872f8f4b2b76
Mark Nagibin <myrkwill@icloud.com>
2022-09-12 15:12:10 +0300

    Dependency Injection

--- src/main/java/ru/myrkwill/springcourse/MusicPlayer.java
+++ src/main/java/ru/myrkwill/springcourse/MusicPlayer.java
@@ -6,12 +6,41 @@ package ru.myrkwill.springcourse;
 public class MusicPlayer {
 
     private Music music;
+    private String name;
+    private int volume;
+
+    public MusicPlayer() {}
 
     public MusicPlayer(Music music) {
         this.music = music;
     }
 
     public void playMusic() {
-        System.out.print("Playing: " + music.getSong());
+        System.out.println("Playing: " + music.getSong());
+    }
+
+
+    public Music getMusic() {
+        return music;
+    }
+
+    public void setMusic(Music music) {
+        this.music = music;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
     }
 }
--- src/main/java/ru/myrkwill/springcourse/TestSpring.java
+++ src/main/java/ru/myrkwill/springcourse/TestSpring.java
@@ -12,9 +12,11 @@ public class TestSpring {
                 "applicationContext.xml"
         );
 
-        Music musicBean = context.getBean("musicBean", Music.class);
-        MusicPlayer musicPlayer = new MusicPlayer(musicBean);
+        MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
         musicPlayer.playMusic();
+
+        System.out.println(musicPlayer.getName());
+        System.out.println(musicPlayer.getVolume());
 
         context.close();
     }
