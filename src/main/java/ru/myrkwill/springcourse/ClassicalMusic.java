04bc844983c761ade5fd7d0788dbd76873462d29
Mark Nagibin <myrkwill@icloud.com>
2022-09-12 16:20:31 +0300

    New methods for Bean

--- src/main/java/ru/myrkwill/springcourse/ClassicalMusic.java
+++ src/main/java/ru/myrkwill/springcourse/ClassicalMusic.java
@@ -5,6 +5,20 @@ package ru.myrkwill.springcourse;
  */
 public class ClassicalMusic implements Music {
 
+    private ClassicalMusic() {}
+
+    public void init() {
+        System.out.println("Init ClassicalMusic");
+    }
+
+    public void deinit() {
+        System.out.println("Deinit ClassicalMusic");
+    }
+
+    public static ClassicalMusic getClassicalMusic() {
+        return new ClassicalMusic();
+    }
+
     @Override
     public String getSong() {
         return "Classic Music";
